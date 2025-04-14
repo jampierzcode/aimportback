81724d2b56d477b65e27153a352ac21604a3dc27
jampierzcode <jampierv127@gmail.com>
2025-04-14 11:14:21 -0500

    arreglos de codigo campaing

--- app/models/campaign.ts
+++ app/models/campaign.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany, hasOne } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import type { HasMany } from '@adonisjs/lucid/types/relations'
 import Pedido from './pedido.js'
 
